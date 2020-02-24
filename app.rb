105710b5c2ad838d7cfcf369f5f57739aae0cd6c
John Ferrry <johntferry@gmail.com>
2020-02-24 05:54:32 +0000

    Commit 7

--- app.rb
+++ app.rb
@@ -13,21 +13,37 @@ get "/" do
 end
 
 get "/newspaper" do
+#get the location
       results = Geocoder.search(params["q"])
-        lat_long = results.first.coordinates # => [lat, long]
+        lat_long = results.first.coordinates
     @location = params["q"]
     @lat = "#{lat_long[0]}"
     @long = "#{lat_long[1]}"
     @lat_long = "#{@lat},#{@long}"
     
+#get the weather
+
 @forecast = ForecastIO.forecast("#{lat_long[0]}","#{lat_long[1]}").to_hash
 
 @current_temp = @forecast["currently"]["temperature"]
 @conditions = @forecast["currently"]["summary"]
 
+    #get the news
 url = "http://newsapi.org/v2/top-headlines?country=us&apiKey=ff8a14f637a549a284639672b2ddddce"
 
 @news = HTTParty.get(url).parsed_response.to_hash
+
+    headline_url = []
+    for headlines in @news["articles"]
+        headline_url << "#{headlines["url"]}"
+    end
+    @topurls = headline_url
+
+    headline_title = []
+    for headlinetitles in @news["articles"]
+        headline_title << "#{headlinetitles["title"]}"
+    end
+    @toptitles = headline_title
 
 # @article_title = news["title"]
 # @article_author = news["author"]
